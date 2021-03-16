6e5489d97e017531e2c06b432acec25ab61c5cbb
Zalan Szugyi <zalan.szugyi@prezi.com>
2021-03-16 12:19:45 +0100

    x

--- .github/actions/approve-checker/index.ts
+++ .github/actions/approve-checker/index.ts
@@ -1,7 +1,9 @@
 import * as core from "@actions/core";
+import * as github from '@actions/github'
 
 const run = async (): Promise<void> => {
 	core.debug("Hello World");
+	console.log({payload: github.context.payload})
 };
 
 run();
